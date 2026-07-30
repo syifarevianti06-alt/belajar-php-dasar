fcaadad3c8338cb8456b076937987a570f7dd7bb
Rodeobazaar124 <azfasa15@gmail.com>
2026-07-30 14:24:53 +0700

    Menambahkan file syifa.php

---
+++ syifa.php
@@ -0,0 +1,57 @@
+<?php
+
+echo "Hi, guyss!";
+
+//tipe data integer
+
+echo "Decimal : ";
+var_dump(1234);
+echo "Octal : ";
+var_dump(0123);
+echo "Hexadecimal : ";
+var_dump(0x1A);
+echo "Binary : ";
+var_dump(0b11111111);
+echo "Underscore di Number : ";
+var_dump(1_234_567);
+
+
+//tipe data float
+
+echo "Floating Point : ";
+var_dump(1.234);
+echo "Floating Point dengan E notation Plus (1.2 x 1000) : ";
+var_dump(1.2e3);
+echo "Floating Point dengan E notation Minu (7 x 0.001) : ";
+var_dump(7e-3);
+echo "Underscore di Floating Point : ";
+var_dump(1_234.567);
+
+//tipe data string
+
+echo 'Nama : ';
+echo 'Ridea Ariyanto';
+
+//tipe data string (double quote)
+
+$nama = "syifa revianti";
+echo "Nama : ";
+echo 'Ridea $nama Ariyanto';
+echo "\n";
+
+//tipe data boolean
+
+echo "Benar";
+echo true;
+
+echo "Salah";
+echo false;
+
+//variabel
+
+$nama = "Syifaaa";
+$age = "17";
+echo " ";
+echo "Name : $nama";
+echo "Age : $age";
+
